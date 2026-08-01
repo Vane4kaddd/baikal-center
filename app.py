3e5ba871b6b6290e753523224ef1da7eb9f53e6d
Vane4kaddd <ivandimov1107@gmail.com>
2026-08-01 18:41:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,10 @@ if "AMVERA" in os.environ:
     DB_HOST = os.environ.get('DB_HOST', 'localhost')
     DB_PORT = os.environ.get('DB_PORT', '5432')
     DB_NAME = os.environ.get('DB_NAME', 'baikal')
-    
+    # ДОБАВИТЬ ЭТУ ДИАГНОСТИКУ:
+    print(f"ИМЯ БАЗЫ (КАК ЕЁ ВИДИТ PYTHON): '{DB_NAME}'")
+    # ------------------------------------------
+
     SQLALCHEMY_DATABASE_URI = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
 else:
     # Локальная разработка — SQLite
